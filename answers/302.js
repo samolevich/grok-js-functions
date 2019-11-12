29c32fca7fcfe800adc905b13d52dfeacf26254a
Eugene Maslovich <ehpc@em42.ru>
2019-11-12 20:54:28 +0300

    Create 302.js

---
+++ answers/302.js
@@ -0,0 +1,12 @@
+function foo() {
+  const x = 42;
+  function bar() {
+    const x = 7;
+  }
+  bar();
+  return x;
+}
+
+foo();
+
+// Значение y = 42 потому что значение x внутри bar является локальным для функции bar и не влияет на x, объявленный в foo.
